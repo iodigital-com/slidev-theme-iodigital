75a0b343006ce1cda541dc8bafa833321a35f140
Remy <remy@remy.io-internal.net>
2023-04-18 14:30:08 +0200

    feat: create and implement base layout

---
+++ layouts/base-layout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    layout: {
+        type: String,
+        required: true,
+    },
+});
+
+const classNames = computed(() => props.layout);
+</script>
+
+<template>
+    <section class="slidev-layout" :class="classNames">
+        <slot></slot>
+    </section>
+</template>
