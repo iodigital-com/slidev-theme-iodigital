46f788fb44366a0c8831201fe181e94da09f35c8
Mart Linger <martlinger@gmail.com>
2023-04-21 09:26:28 +0200

    style: use only tailwind classes

--- layouts/quote.vue
+++ layouts/quote.vue
@@ -10,9 +10,7 @@
 
 <style scoped>
 	.quote {
-		@apply w-1/2 fixed top-[50%] ;
-	 	background-color: transparent;
-		border: 0;
+		@apply w-1/2 fixed top-[50%] bg-transparent border-0;
 
 		&:deep(p) {
 			@apply text-3xl font-quote leading-[2.2rem];
