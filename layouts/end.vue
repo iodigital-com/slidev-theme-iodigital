dc709eeb3921cfd3660776466cf36ad085d23197
Mart Linger <martlinger@gmail.com>
2023-04-26 13:54:22 +0200

    feat: style end layout

---
+++ layouts/end.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+	<div class="grid place-content-center slidev-layout bg-blend-purple text-center">
+		<h1>Infinite Oppurtunities</h1>
+	</div>
+</template>
