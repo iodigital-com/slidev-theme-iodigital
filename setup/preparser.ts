e4d19885d318cc5cda9eb774e7e5c656ac4b819c
Mart Linger <Lingertje@users.noreply.github.com>
2023-05-10 14:26:14 +0200

    Feat/83 only show main sections in the toc (#84)
    * feat: only show sections in ToC
    
    * feat: allow manual override of the hideInToc
    
    * style: replace toc screenshot and formatting
    
    * feat: check is section is not set to false

---
+++ setup/preparser.ts
@@ -0,0 +1,25 @@
+import { definePreparserSetup } from '@slidev/types'
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export default definePreparserSetup(() => {
+    return [{
+        transformSlide(content, frontmatter) {
+            // Early return if hideInToc (Table of Content) is manually set in frontmatter
+            if ('hideInToc' in frontmatter) return content;
+
+            // Default all slides to hideInToc
+            frontmatter.hideInToc = true;
+
+            if ('layout' in frontmatter) {
+                // Show only slides in ToC with layout: section, section is not set to false and level: 1 or undefined
+                if (frontmatter.layout === 'section' && frontmatter.section !== false && (frontmatter.level === 1 || frontmatter.level === undefined)) {
+                    frontmatter.hideInToc = false;
+                }
+
+                return content;
+            }
+        },
+    }]
+})
+
