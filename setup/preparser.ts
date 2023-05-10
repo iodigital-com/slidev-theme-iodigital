8e0d6f4888d164c0069473ff3d97a55eba1f8542
Mart Linger <martlinger@gmail.com>
2023-05-10 14:07:00 +0200

    style: replace toc screenshot and formatting

--- setup/preparser.ts
+++ setup/preparser.ts
@@ -3,25 +3,23 @@ import { definePreparserSetup } from '@slidev/types'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 export default definePreparserSetup(() => {
-  return [
-    {
-      transformSlide(content, frontmatter) {
-        // Early return if hideInToc (Table of Content) is manually set in frontmatter
-        if ('hideInToc' in frontmatter) return content;
+    return [{
+        transformSlide(content, frontmatter) {
+            // Early return if hideInToc (Table of Content) is manually set in frontmatter
+            if ('hideInToc' in frontmatter) return content;
 
-        // Default all slides to hideInToc
-        frontmatter.hideInToc = true;
+            // Default all slides to hideInToc
+            frontmatter.hideInToc = true;
 
-        if ('layout' in frontmatter) {
-            // Show only slides in ToC with layout: section and level: 1 or undefined
-            if (frontmatter.layout === 'section' && (frontmatter.level === 1 || frontmatter.level === undefined)) {
-                frontmatter.hideInToc = false;
+            if ('layout' in frontmatter) {
+                // Show only slides in ToC with layout: section and level: 1 or undefined
+                if (frontmatter.layout === 'section' && (frontmatter.level === 1 || frontmatter.level === undefined)) {
+                    frontmatter.hideInToc = false;
+                }
+
+                return content;
             }
-
-            return content;
-        }
-      },
-    },
-  ]
+        },
+    }]
 })
 
